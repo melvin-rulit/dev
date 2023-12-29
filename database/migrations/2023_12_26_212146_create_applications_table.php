fcb490a502ca06112867c31101858dd878914816
melvin-rulit <melvin.melvin.rulit@gmail.com>
2023-12-29 15:52:58 +0200

    добавляю миграцию и модель

---
+++ database/migrations/2023_12_26_212146_create_applications_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\Application;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->enum('status', array_keys(Application::STATUS));
+            $table->string('message');
+            $table->text('comment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('applications');
+    }
+};
